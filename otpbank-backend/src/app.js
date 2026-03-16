b10c5948e88777cd328ad4a1add1cd400faac1f0
Savva <72355374+savva643@users.noreply.github.com>
2026-03-16 12:25:00 +0400

    Update app.js

--- otpbank-backend/src/app.js
+++ otpbank-backend/src/app.js
@@ -3,6 +3,11 @@ const { router } = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 const path = require('path');
 
+const allowedOrigins = new Set([
+  'http://otpbank.keep-pixel.ru',
+  'https://otpbank.keep-pixel.ru'
+]);
+
 function createApp() {
   const app = express();
 
@@ -16,6 +21,25 @@ function createApp() {
   });
 
   app.use(express.json({ limit: '1mb' }));
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin && allowedOrigins.has(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader('Vary', 'Origin');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, accept, authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.status(204).end();
+      return;
+    }
+
+    next();
+  });
 
   app.use('/static', express.static(path.join(__dirname, '..', 'public')));
   app.use('/api/static', express.static(path.join(__dirname, '..', 'public')));
